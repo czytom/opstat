ecc1a223c1d6b8ead0278f985faeda86c0f33f93
Krzysztof Tomczyk <czytom@gmail.com>
2014-12-18 22:53:46 +0100

    extract parsers to common opstat-plugins gem

--- lib/opstat-master/parsers.rb
+++ lib/opstat-master/parsers.rb
@@ -7,14 +7,7 @@ module Parsers
       @parsers = {}
     end
     def load_parsers
-      pwd  = File.dirname(File.expand_path(__FILE__))
-      Dir.glob(File.expand_path("#{pwd}/parsers/*.rb")).each do |file|
-        require file
-	plugin_name = File.basename(file,'.*')
-	plugin_class = "Opstat::Parsers::#{plugin_name.capitalize}"
-	oplogger.info "loading parser #{plugin_name}"
-        @parsers[plugin_name] ||= Opstat::Common.constantize(plugin_class).new 
-      end
+      Opstat::Plugins.load_parsers
     end
 
     def parse_and_save(params)
--- opstat-master.gemspec
+++ opstat-master.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_dependency "opstat-plugins", Opstat::VERSION
   spec.add_dependency "eventmachine"
   spec.add_dependency "json"
   spec.add_dependency "amqp"

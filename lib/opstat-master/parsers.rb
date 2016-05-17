80bd805db98275c6ed8bbe2f1735811b2b207c2f
Krzysztof Tomczyk <czytom@gmail.com>
2016-05-17 12:29:26 +0200

    modify gem dependencies, add disk_io

--- lib/opstat-master/parsers.rb
+++ lib/opstat-master/parsers.rb
@@ -10,15 +10,31 @@ module Parsers
       @parsers = Opstat::Plugins.load_parsers(oplogger)
     end
 
+    def influxdb
+      database = 'opstat'
+      username = 'opstat'
+      password = 'po123'
+      time_precision = 's'
+      @influxdb ||= InfluxDB::Client.new database, username: username, password: password, time_precision: time_precision
+    end
+
     def parse_and_save(params)
       plugin = params[:plugin]
       host = params[:host]
       data = params[:plugin_data]['data']
       time = Time.parse(params[:plugin_data]['timestamp'])
-      reports = @parsers[plugin.type].parse_data(data)
       oplogger.info "Saving parsed data collected on #{time} from #{host.id}(plugin:#{plugin.type} #{host.hostname}) "
+      reports = @parsers[plugin.type].parse_data(data)
+      #TODO save errors to db
+      return if reports.nil?
       reports.each do |report|
         rp = report.merge({ :timestamp => time, :host_id => host.id, :plugin_id => plugin.id, :plugin_type => plugin.type})
+	if plugin.type == 'load' or plugin.type == 'cpu' or plugin.type == 'disk_io'
+	  puts 'WRITING INFLUXDB'
+	  require 'influxdb'
+	  rp2 = { :values => report, :timestamp => time.to_i, :tags => { :host_id => host.id, :plugin_id => plugin.id }, :name => plugin.type }
+	  influxdb.write_point(plugin.type, rp2)
+	end
         Report.create(rp)
       end
     end

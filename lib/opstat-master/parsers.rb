a615385e9fd199238ed7b94f81437b4ce8e3d62a
Krzysztof Tomczyk <czytom@gmail.com>
2016-10-25 14:54:23 +0200

    remove influx

--- lib/opstat-master/parsers.rb
+++ lib/opstat-master/parsers.rb
@@ -10,14 +10,6 @@ module Parsers
       @parsers = Opstat::Plugins.load_parsers(oplogger)
     end
 
-    def influxdb
-      database = 'opstat'
-      username = 'opstat'
-      password = 'po123'
-      time_precision = 's'
-      @influxdb ||= InfluxDB::Client.new database, username: username, password: password, time_precision: time_precision
-    end
-
     def parse_and_save(params)
       plugin = params[:plugin]
       host = params[:host]
@@ -29,12 +21,6 @@ module Parsers
       return if reports.nil?
       reports.each do |report|
         rp = report.merge({ :timestamp => time, :host_id => host.id, :plugin_id => plugin.id, :plugin_type => plugin.type})
-	if plugin.type == 'load' or plugin.type == 'cpu' or plugin.type == 'disk_io'
-	  puts 'WRITING INFLUXDB'
-	  require 'influxdb'
-	  rp2 = { :values => report, :timestamp => time.to_i, :tags => { :host_id => host.id, :plugin_id => plugin.id }, :name => plugin.type }
-	  influxdb.write_point(plugin.type, rp2)
-	end
         Report.create(rp)
       end
     end

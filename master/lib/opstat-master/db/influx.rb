d559cc550bf6621464c72b13f6767138661c420c
Krzysztof Tomczyk <czytom@gmail.com>
2019-09-17 10:10:23 +0200

    manage influx timeouts during saving data

--- master/lib/opstat-master/db/influx.rb
+++ master/lib/opstat-master/db/influx.rb
@@ -2,6 +2,7 @@ module Opstat
 module DB
   class Influx
     include Singleton
+    include Opstat::Logging
     def initialize
       @config = Opstat::Config.instance.get_influx_config
       @influxdb = InfluxDB::Client.new @config
@@ -9,7 +10,14 @@ module DB
     end
 
     def write_point(name, measurement)
-      @influxdb.write_point(name, measurement)
+      begin
+        retries ||= 0
+        oplogger.info "writing stats problem saving data - try ##{ retries }"
+        @influxdb.write_point(name, measurement)
+      rescue InfluxDB::Error
+        sleep 5
+        retry if (retries += 1) < 3
+      end
     end
   end
 end

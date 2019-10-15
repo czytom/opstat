7413dd8a8a47cd6e8f83b73f4f19d5309264c915
Krzysztof Tomczyk <czytom@gmail.com>
2019-10-15 14:05:20 +0200

    rewrite haproxy parser, add check to disk_io parser

--- plugins/lib/parsers/disk.rb
+++ plugins/lib/parsers/disk.rb
@@ -38,6 +38,7 @@ module Parsers
 
     def get_disk_io_stats_for_device(device, data)
       raw_disk_io_line = data.grep(/#{device}/).first
+      return {} if raw_disk_io_line.nil?
       raw_disk_io_line.match(/^\s+(?<major_number>\d+)\s+(?<minor_number>\d+)\s+\S+\s+(?<reads_completed>\d+)\s+(?<reads_merged>\d+)\s+(?<sector_read>\d+)\s+(?<time_spent_reading_ms>\d+)\s+(?<writes_completed>\d+)\s+(?<writes_merged>\d+)\s+(?<sectors_written>\d+)\s+(?<time_spent_writing>\d+)\s+(?<io_in_progress>\d+)\s+(?<time_spent_doing_io_ms>\d+)\s+(?<weighted_time_doing_io>\d+)\s+.*/)
       unless $~.nil?
         return $~.names.zip($~.captures).map{|m| [m[0],m[1].to_i]}.to_h
--- plugins/lib/parsers/haproxy.rb
+++ plugins/lib/parsers/haproxy.rb
@@ -6,25 +6,14 @@ module Parsers
     include Opstat::Logging
 
     def parse_data(data:, time:)
+      reports = []
       white_headers = [ :svname, :qcur, :qmax, :scur, :smax, :slim, :stot, :bin, :bout, :hrsp_1xx, :hrsp_2xx, :hrsp_3xx, :hrsp_4xx, :hrsp_5xx, :hrsp_other, :req_tot, :conn_tot]
-      parsed_data = CSV.parse(data.join, { headers: true, header_converters: :symbol, converters: :all}).map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
-      report = []  
-      yaml = []
-      parsed_data.each do |key,value| 
-        if value.first[:svname] == "FRONTEND"
-          report <<  {:stats_type => :frontend, :name => value.first[:_pxname], :summary => value.first.select { |key, value| white_headers.include?(key)}}
-        elsif value.first[:_pxname].to_s.start_with?("backend")
-          backends = { :stats_type => :backend, :name => value.first[:_pxname] }
-          instances = []
-          value.each do |backend|
-            if backend[:svname]  == "BACKEND"
-              backends[:summary] =  backend.select { |key, value| white_headers.include?(key) }
-            else
-              backends[:details] = instances.push ( backend.select { |key, value| white_headers.include?(key) } )
-            end
-          end
-          report <<  backends
-        end
+      parsed_data = CSV.parse(data.join, { headers: true, header_converters: :symbol, converters: :all})#.map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
+      parsed_data.each do |row|
+        report = row.to_hash
+        report['OPSTAT_TAG_svname'] = report.delete(:svname)
+        report['OPSTAT_TAG_pxname'] = report.delete(:pxname)
+        reports << report
       end
       return report
     end

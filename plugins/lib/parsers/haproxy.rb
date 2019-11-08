7e7cbcfeb3ed62cda0f529ac1b00f48b86bc81f2
Krzysztof Tomczyk <czytom@gmail.com>
2019-11-08 12:38:35 +0100

    remove gui parts as we use grafana now

--- plugins/lib/parsers/haproxy.rb
+++ plugins/lib/parsers/haproxy.rb
@@ -11,11 +11,14 @@ module Parsers
       parsed_data = CSV.parse(data.join, { headers: true, header_converters: :symbol, converters: :all})#.map{|row| Hash[row.headers[0..-1].zip(row.fields[0..-1])]}.group_by{|row| row[:_pxname]}
       parsed_data.each do |row|
         report = row.to_hash
-        report['OPSTAT_TAG_svname'] = report.delete(:svname)
-        report['OPSTAT_TAG_pxname'] = report.delete(:pxname)
+        report["OPSTAT_TAG_svname"] = report.delete(:svname)
+        report["OPSTAT_TAG_pxname"] = report.delete(:pxname)
+        report.each_pair do |key,value|
+          report.delete(key) if value.nil?
+        end
         reports << report
       end
-      return report
+      return reports
     end
   end
 end

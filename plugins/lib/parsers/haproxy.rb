f118b8f8fbc7a147b08ebbf3270835a3b9a90d2c
Krzysztof Tomczyk <czytom@gmail.com>
2019-11-19 14:39:20 +0100

    update haproxy tests

--- plugins/lib/parsers/haproxy.rb
+++ plugins/lib/parsers/haproxy.rb
@@ -10,8 +10,8 @@ module Parsers
       parsed_data = CSV.parse(data.join, { headers: true, header_converters: :symbol, converters: :all})
       parsed_data.each do |row|
         report = row.to_hash
-        report["OPSTAT_TAG_svname"] = report.delete(:svname)
-        report["OPSTAT_TAG_pxname"] = report.delete(:pxname)
+        report[:OPSTAT_TAG_svname] = report.delete(:svname)
+        report[:OPSTAT_TAG_pxname] = report.delete(:pxname)
         report.each_pair do |key,value|
           report.delete(key) if value.nil?
         end

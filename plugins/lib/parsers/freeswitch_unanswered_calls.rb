e6aa16a36041229a3fdf3f3ca4fcbe9564b73f0a
Krzysztof Tomczyk <ktomczyk@ifirma.pl>
2023-10-25 10:02:05 +0200

    add unanswered plugin

---
+++ plugins/lib/parsers/freeswitch_unanswered_calls.rb
@@ -0,0 +1,21 @@
+require 'time'
+
+module Opstat
+module Parsers
+  class FreeswitchCalls
+    include Opstat::Logging
+
+    def parse_data(data:, time:)
+      reports = []  
+      parsed_data = CSV.parse(data, { headers: true, header_converters: :symbol, converters: :all})
+      parsed_data.each do |row|
+        report = {:time => row[:time], :tags => {}, :values => {
+          :caller_id_number => row[:caller_id_number], :duration => row[:duration], :billsec => row[:billsec], : :answer_stamp => row[:answer_stamp].to_time.to_i, :end_stamp => row[:end_stamp].to_time.to_i
+        }}
+        reports << report
+      end
+      return reports
+    end
+  end
+end
+end 
---
+++ plugins/lib/plugins/freeswitch_unanswered_calls.rb
@@ -0,0 +1,28 @@
+require 'erb'
+
+module Opstat
+module Plugins
+class FreeswitchUnansweredCalls < Task
+  def initialize (name, queue, config)
+    super(name, queue, config)
+    @calls_cmd_template = config['cmd_template']
+    self
+  end
+
+  def parse
+    @count_number += 1
+    report = `#{calls_get_cmd}`
+    return report
+  end
+
+  def calls_get_cmd
+    ERB.new(@calls_cmd_template).result( binding )
+  end
+
+  def sql
+    sql = "SELECT inbound.caller_id_number, inbound.destination_number, datetime(inbound.start_stamp,'utc') as start_time, datetime(inbound.answer_stamp,'utc') as answer_time, datetime(inbound.end_stamp,'utc') as end_stamp, inbound.duration, inbound.billsec FROM cdr inbound left join cdr out on inbound.uuid=out.bleg_uuid  WHERE inbound.context='power-fifos' and inbound.start_stamp >= date('now') and out.start_stamp is null order by inbound.start_stamp asc;"
+    return sql
+  end
+end
+end
+end
